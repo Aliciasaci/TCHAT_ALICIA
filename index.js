a44612a00938071092d2b76679449b5b55ec8b15
alicia <aliciasaci@gmail.com>
2021-11-26 10:36:29 +0100

    version initiale du tchat

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Express est le serveur web utilisé
+ *
+ */
+const express = require('express');
+//instancier le serveur express
+const app = express();
+const http = require('http');
+//création d'une instance de http qui utilise l'instance de express
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+//Créer la route / 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//Ecouter l'evenement "connetion" de socket.io
+io.on('connection', (socket) => {
+    console.log("Un utilisateur s'est connecté")
+
+    //Ecouter les déconnexion
+    socket.on("disconnect", () => {
+        console.log("Un utilisateur s'est déconnecté");
+    });
+
+    //Ecouter les messages échangés(Réception du message emi du côté client)
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+//Demander au serveru HTPP de répondre sur le port 3000
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
